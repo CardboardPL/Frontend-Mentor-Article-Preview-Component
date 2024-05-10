3c40210c680c02464e3289ba9d6ad02a82ee7e0c
CardboardPL <mrplwaschilling@gmail.com>
2024-05-10 15:25:01 +0800

    Added/Updated: toggleVisibility.js index.html
    In this commit:
    - Added toggleVisibility.js to implement the functionality of the share-btn when clicked.
    - Updated index.html to update the structure for further styling and to link the toggleVisibility.js file.

---
+++ scripts/toggleVisibility.js
@@ -0,0 +1,12 @@
+const shareBtn = document.querySelector('.share-btn');
+const sharePopup = document.querySelector('.share-popup');
+
+function toggleVisibility(element) {
+  element.classList.toggle('hidden');
+}
+
+shareBtn.addEventListener('click', () => {
+  toggleVisibility(sharePopup);
+  shareBtn.setAttribute('data-active', shareBtn.getAttribute('data-active') !== 'true');
+  sharePopup.setAttribute('aria-hidden', sharePopup.getAttribute('aria-hidden') !== 'true');
+})
